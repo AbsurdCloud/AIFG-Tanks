00e22128abaae65124477e5eca0ea50d3afbd0bc
Andrei <andreimazilu20@gmail.com>
2018-04-30 17:44:12 +0100

    check for score and visible squares

--- AIFG-TanksSolution/TankyMcTankFace.cs
+++ AIFG-TanksSolution/TankyMcTankFace.cs
@@ -25,6 +25,15 @@ namespace GridWorld
             // 0 - Up, 1-Down, 2- Left, 3-Right, 4-RotateLeft, 5-RotateRight, 6-Stay
             Random rand = new Random();
             int direction = rand.Next(7);
+            for (int i=0; i<myWorldState.MyVisibleSquares.Count; i++)
+            {
+                WriteTrace(myWorldState.MyVisibleSquares[i] + " ");
+            }
+
+            //WriteTrace("ScoreEmptySquare " + myWorldState.ScorePerEmptySquareSeen);
+           // WriteTrace("ScoreTankDestroyed " + myWorldState.ScorePerOpposingTankDestroyed);
+            //WriteTrace("ScoreRockSquare " + myWorldState.ScorePerRockSquareSeen);
+
             switch (direction)
             {
                 case 0: return new Command(Command.Move.Up, false);
